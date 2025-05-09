0305d1a82895c37cd3e5cb35395ff49d1a2cae2a
timahern <71024140+timahern@users.noreply.github.com>
2025-05-09 12:59:23 -0400

    added testing functionality for bucket list screen

---
+++ bucket_list_app/test/bucket_list_screen_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bucket_list_app/screens/bucket_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:bucket_list_app/models/bucket_list.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  testWidgets('renders bucket list title and FAB', (WidgetTester tester) async {
+    final mockUserId = 'test-user-123';
+    final fakeFirestore = FakeFirebaseFirestore();
+
+    // Add fake bucket list doc to Firestore
+    final bucketListDoc = await fakeFirestore.collection('bucket_lists').add({
+      'title': 'Test List',
+      'userId': mockUserId,
+      'items': [],
+    });
+
+    final mockBucketList = BucketList(
+      id: bucketListDoc.id,
+      title: 'Test List',
+      userId: mockUserId,
+      items: [],
+    );
+
+    await tester.pumpWidget(MaterialApp(
+      home: BucketListScreen(
+        bucketList: mockBucketList,
+        onUpdate: () {},
+      ),
+    ));
+
+    expect(find.text('Test List'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+}
